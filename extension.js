67db77566377aa95d5d643535e2febd96385e005
Puukjeens <klaza@tvnet.lv>
2015-07-04 18:44:30 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,16 +34,16 @@
          }
 
          */
-
-        bot.commands.PuukjeensCommand = {
-            command: 'Puukjeens',  //The command to be called. With the standard command literal this would be: !bacon
+         
+ 	bot.commands.baconCommand = {
+            command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me Puukjeens Jūsu Rīcībā!!!");
+                    API.sendChat("/me Bacon!!!");
                 }
             }
         };
